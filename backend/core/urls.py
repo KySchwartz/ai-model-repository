025f510761efab9562ed1ccc51544cceaf9b4cba
unknown <jsamples1121@gmail.com>
2026-02-23 20:47:47 -0600

    Added AIModel upload functionality

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
+from django.contrib.auth import views as auth_views 
+ 
+urlpatterns = [ 
+   path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), 
+   path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
 ]
