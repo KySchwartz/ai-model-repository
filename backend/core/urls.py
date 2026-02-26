bd2a290cdf36c58b680b3d846b56d7e1b4e84475
KySchwartz <kyboo321@gmail.com>
2026-02-25 20:34:51 -0600

    Fixed path to admin login

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -19,6 +19,7 @@ from . import views
 from django.contrib.auth import views as auth_views 
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     # Home Path 
@@ -34,6 +35,9 @@ urlpatterns = [
     path("signup/", views.signup_view, name="signup"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
+
+    #Admin Path
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
